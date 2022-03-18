16766394e12b17f9bbfc9a473855e46ec0253aa3
muhammadgata01 <gataluthfi@refactory.id>
2022-03-18 22:59:16 +0700

    setup routing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Discover from './pages/Discover';
+import Create from './pages/Create';
+
 function App() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-dark-600 to-dark-800 text-lg text-white antialiased">
-      <div className="container">Content</div>
+      <Routes>
+        <Route path="/" element={<Discover />} />
+        <Route path="/create" element={<Create />}></Route>
+      </Routes>
     </main>
   );
 }
---
+++ client/src/pages/Discover.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Discover() {
+  return <div>DiscoverView</div>;
+}
+
+export default Discover;
