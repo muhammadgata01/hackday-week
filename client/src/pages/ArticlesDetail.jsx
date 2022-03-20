fcf3323889092c2071b4b3490cdaf8b37ab4a433
muhammadgata01 <gataluthfi@refactory.id>
2022-03-20 21:56:13 +0700

    get single posts

--- client/src/pages/ArticlesDetail.jsx
+++ client/src/pages/ArticlesDetail.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function ArticlesDetail() {
+  const [post, setPost] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    const baseUrl = 'http://localhost:5000/v1/api';
+    axios.get(`${baseUrl}/posts/${params.id}`).then(response => {
+      setPost(response.data);
+    });
+  }, []);
+
   return (
     <article className="flex flex-col gap-12 py-10">
       <div className="flex flex-col items-center">
-        <span>React Native &bull; 19 Maret 2021</span>
-        <h2 className="my-4 w-10/12 text-center lg:w-6/12">
-          The More Important the Work, the More Important the Rest
-        </h2>
+        <span>
+          {post.tag} &bull; {post.updatedAt}
+        </span>
+        <h2 className="my-4 w-10/12 text-center lg:w-6/12">{post.title}</h2>
         <div className="flex items-center gap-4">
           <img
             src={
@@ -24,37 +36,13 @@ function ArticlesDetail() {
       </div>
       <figure className="flex justify-center">
         <img
-          src={
-            'https://ik.imagekit.io/3t4mp2a1w30/image-detail_HhApMyhGg.png?ik-sdk-version=javascript-1.4.3&updatedAt=1647684236940'
-          }
-          className="h-auto w-full lg:w-10/12"
+          src={`/images/${post.image}`}
+          className="h-[26rem] w-full lg:w-8/12"
           alt="Image Detail"
         />
       </figure>
       <div className="flex flex-col items-center gap-6">
-        <p className="w-10/12 lg:w-8/12">
-          Male sixth sea it a. Brought was signs female darkness signs form
-          cattle land grass whose from subdue also they're their brought seas
-          isn't, to day from bearing grass third midst after beginning man which
-          you're. Dry, gathering beginning given made them evening.
-        </p>
-        <p className="w-10/12 lg:w-8/12">
-          Lights dry. Thing, likeness, forth shall replenish upon abundantly our
-          green. Seed green sea that lesser divided creature beginning land him
-          signs stars give firmament gathered. Wherein there their morning a he
-          grass. Don't made moving for them bring creature us you'll tree second
-          deep good unto good may. Us yielding.
-        </p>
-        <p className="w-10/12 lg:w-8/12">
-          Have. Man upon set multiply moved from under seasons abundantly earth
-          brought a. They're open moved years saw isn't morning darkness. Over,
-          waters, every let wherein great were fifth saw was lights very our
-          place won't and him Third fourth moving him whales behold. Beast
-          second stars lights great was don't green give subdue his. Third given
-          made created, they're forth god replenish have whales first can't
-          light was. Herb you'll them beast kind divided. Were beginning fly air
-          multiply god Yielding sea don't were forth.
-        </p>
+        <p className="w-10/12 lg:w-8/12">{post.body}</p>
       </div>
     </article>
   );
