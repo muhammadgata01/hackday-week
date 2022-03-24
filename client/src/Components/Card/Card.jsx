33999b5c7e0870046f20d9ad0591327a11434d96
muhammadgata01 <gataluthfi@refactory.id>
2022-03-24 21:05:55 +0700

    remove image public

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -6,8 +6,10 @@ const Card = ({ data, isLarge }) => {
     <article className={isLarge ? 'grid gap-8 lg:grid-cols-3' : ''}>
       <figure className={isLarge ? 'lg:col-span-2' : ''}>
         <img
-          src={`/images/${data.image}`}
-          className={isLarge ? 'h-[26rem] w-full' : 'h-auto '}
+          src={data.image}
+          className={
+            isLarge ? 'h-[26rem] w-full rounded-2xl' : 'w-full h-60 rounded-xl'
+          }
           alt="Card"
         />
       </figure>
